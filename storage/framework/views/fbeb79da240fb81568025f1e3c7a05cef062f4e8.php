4b3f36273c53165cb7bc0de2e45a02b5c0ebe406
Charles Combs <charlescombs@Charless-MacBook-Pro.local>
2016-09-08 16:23:40 -0400

    destroying

---
+++ storage/framework/views/fbeb79da240fb81568025f1e3c7a05cef062f4e8.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: charlescombs
+ * Date: 9/1/16
+ * Time: 9:10 AM
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <link href="<?php echo e(elixir('css/app.css')); ?>" rel="stylesheet" type="text/css">
+    <!-- Styles -->
+    
+
+    
+            
+            
+            
+            
+            
+            
+            
+            
+        
+
+        
+    
+        
+
+        
+    
+            
+            
+        
+
+        
+    
+
+
+        
+    
+    
+            
+        
+
+        
+    
+        
+
+        
+    
+        
+
+        
+    
+    
+            
+            
+            
+            
+            
+        
+
+        
+    
+        
+
+        
+
+    
+
+
+
+
+        
+
+    
+            
+            
+            
+            
+            
+
+        
+
+        
+
+    
+            
+            
+            
+            
+            
+
+        
+
+    
+
+
+</head>
+<body>
+
+<div class="container">
+    <div class="row flex-items-xs-left">
+        <div class="col-xs-4">
+            One of two columns
+        </div>
+        <div class="col-xs-4">
+            One of two columns
+        </div>
+    </div>
+    <div class="row flex-items-xs-center">
+        <div class="col-xs-4">
+            One of two columns
+        </div>
+        <div class="col-xs-4">
+            One of two columns
+        </div>
+    </div>
+    <div class="row flex-items-xs-right">
+        <div class="col-xs-4">
+            One of two columns
+        </div>
+        <div class="col-xs-4">
+            One of two columns
+        </div>
+    </div>
+    <div class="row flex-items-xs-around">
+        <div class="col-xs-4">
+            One of two columns
+        </div>
+        <div class="col-xs-4">
+            One of two columns
+        </div>
+    </div>
+    <div class="row flex-items-xs-between">
+        <div class="col-xs-4">
+            One of two columns
+        </div>
+        <div class="col-xs-4">
+            One of two columns
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
